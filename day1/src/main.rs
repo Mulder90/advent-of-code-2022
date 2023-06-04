d0b855e424394be6f60bc3a3ec35d26b2100c96d
Lorenzo Cinque <lore.cinque@gmail.com>
2023-06-04 22:05:03 +0100

    Add day1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,5 +1,10 @@
 fn main() {
     let file = std::fs::read_to_string("./input.txt").unwrap();
+    part1(&file);
+    part2(&file);
+}
+
+fn part1(file: &str) {
     let result = file
         .split("\n\n")
         .map(|load| {
@@ -10,4 +15,18 @@ fn main() {
         .max()
         .unwrap();
     println!("{}", result.to_string());
+}
+
+fn part2(file: &str) {
+    let mut grouped_loads: Vec<u32> = file
+        .split("\n\n")
+        .map(|load| {
+            load.lines()
+                .map(|item| item.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .collect();
+    grouped_loads.sort();
+    let total_calories = grouped_loads.iter().rev().take(3).sum::<u32>();
+    println!("{}", total_calories);
 }
